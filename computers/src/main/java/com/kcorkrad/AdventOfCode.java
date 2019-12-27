03594611f5d271fdc1bffa165bf0ebbcbdcb8082
Doug Rogers <drogers@netroadshow.com>
2019-12-27 13:33:56 -0500

    day 2.2 progress - error

--- computers/src/main/java/com/kcorkrad/AdventOfCode.java
+++ computers/src/main/java/com/kcorkrad/AdventOfCode.java
@@ -27,12 +27,32 @@ public class AdventOfCode {
 
         OpCodeSys opCodeSys = new OpCodeSys();
 
-        System.out.println("OpCode Result: \n" + Arrays.toString(opCodeSys.processIntCode(OPCODE_ARRAY)));
+        Integer[] opCodeArrayWorkingCopy = OPCODE_ARRAY;
+        opCodeArrayWorkingCopy[1] = 12;
+        opCodeArrayWorkingCopy[2] = 2;
+
+
+        System.out.println("OpCode Result Day 2.1: \n" + Arrays.toString(opCodeSys.processIntCode(opCodeArrayWorkingCopy)));
+        // answer: 6627023
+
+        // day 2.2
+        Integer desiredOutput = 19690720;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                // reset
+                opCodeArrayWorkingCopy = OPCODE_ARRAY;
+                opCodeArrayWorkingCopy[i] = 12;
+                opCodeArrayWorkingCopy[j] = 2;
+                if (opCodeSys.processIntCode(opCodeArrayWorkingCopy)[0] == desiredOutput) {
+                    System.out.println("100 * noun + verb = " + (100 * i + j));
+                }
+            }
+        }
     }
 
     // original code from day 2.1 was 1,0,0,3,1,1,2,3...
     static Integer[] OPCODE_ARRAY = {
-            1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,9,19,23,1,23,5,27,2,27,10,31,1,6,31,35,1,6,35,39,2,9,39,43,1,6,43,47,1,47,5,51,1,51,13,55,1,55,13,59,1,59,5,63,2,63,6,67,1,5,67,71,1,71,13,75,1,10,75,79,2,79,6,83,2,9,83,87,1,5,87,91,1,91,5,95,2,9,95,99,1,6,99,103,1,9,103,107,2,9,107,111,1,111,6,115,2,9,115,119,1,119,6,123,1,123,9,127,2,127,13,131,1,131,9,135,1,10,135,139,2,139,10,143,1,143,5,147,2,147,6,151,1,151,5,155,1,2,155,159,1,6,159,0,99,2,0,14,0
+            1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,9,19,23,1,23,5,27,2,27,10,31,1,6,31,35,1,6,35,39,2,9,39,43,1,6,43,47,1,47,5,51,1,51,13,55,1,55,13,59,1,59,5,63,2,63,6,67,1,5,67,71,1,71,13,75,1,10,75,79,2,79,6,83,2,9,83,87,1,5,87,91,1,91,5,95,2,9,95,99,1,6,99,103,1,9,103,107,2,9,107,111,1,111,6,115,2,9,115,119,1,119,6,123,1,123,9,127,2,127,13,131,1,131,9,135,1,10,135,139,2,139,10,143,1,143,5,147,2,147,6,151,1,151,5,155,1,2,155,159,1,6,159,0,99,2,0,14,0
     };
 
     static Integer[] INPUTARRAY = {
